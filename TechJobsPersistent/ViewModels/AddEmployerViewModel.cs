e5fb0efe92cf9d69714bbed5a2dbefedcaeb2c5a
Dennis Sheets <dennissheets@outlook.com>
2022-01-09 23:41:21 -0600

    completed adding Employers ViewModel, Controller Update, and 3 Views

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage ="Name is Required")]
+        [StringLength(30,MinimumLength =5)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Location is Required")]
+        [StringLength(30, MinimumLength = 5)]
+        public string Location { get; set; }
+
+        public AddEmployerViewModel() { }
+        public AddEmployerViewModel(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,17 @@
---Part 1
+-- Part 1
+-- https://thispointer.com/mysql-get-column-names-and-datatypes-of-a-table/
+
+SELECT COLUMN_NAME , DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = Database()
+AND TABLE_NAME = 'jobs' ;
+
 
 --Part 2
+
+
+
 
 --Part 3
 
